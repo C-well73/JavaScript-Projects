8df1f55b74cfe5022e8dbfc07bd03d41766ba70a
C-well73 <49219055+C-well73@users.noreply.github.com>
2020-04-27 20:51:25 -0400

    Add files via upload

---
+++ BasicJavaScript_Projects/JS/Basic_JavaScript_1.js
@@ -0,0 +1,28 @@
+function myFunction() {
+    alert("Hello, My name is Bryant!");
+}
+var Family = "The Arezzinis", Dad = "Jeremiah", Mom = "Hermoine", Daughter = "Penny", Son ="Zorro";
+var Family = Family.fontcolor("red");
+var Dad = Dad.fontcolor("blue");
+var Mom = Mom.fontcolor("green");
+var Daughter = Daughter.fontcolor("orange");
+var Son = Son.fontcolor("black");
+
+document.write(Family);
+document.write(Dad);
+document.write(Mom);
+document.write(Daughter);
+document.write(Son);
+
+var firstName = "Bryant";
+var lastName = "Caldwell";
+
+document.write(firstName + lastName);
+
+var firstName = firstName.fontcolor("red");
+var lastName = lastName.fontcolor("blue");
+
+document.write(firstName);
+document.write(lastName);
+
+console.log(2 + 3 - 4);
